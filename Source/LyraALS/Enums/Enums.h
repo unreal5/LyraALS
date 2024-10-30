b569ad974d7eb1712070303415e6afebee488a6e
unreal5 <lingbao@outlook.com>
2024-10-30 23:44:03 +0800

    0057 n/a

--- Source/LyraALS/Enums/Enums.h
+++ Source/LyraALS/Enums/Enums.h
@@ -18,3 +18,13 @@ enum class EGait : uint8
 	Jogging,
 	EG_Max UMETA(Hidden)
 };
+
+UENUM(BlueprintType)
+enum class ELocomotionDirection : uint8
+{
+	Forward UMETA(DisplayName = "前"),
+	Backward UMETA(DisplayName = "后"),
+	Right UMETA(DisplayName = "右"),
+	Left UMETA(DisplayName = "左"),
+	ELD_Max UMETA(Hidden)
+};
