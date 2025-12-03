8b5ac375282e8ce45d72840d76b0a240c8fcbe98
lingbao <lingbao@outlook.com>
2025-12-03 09:27:04 +0800

    6. 05.Blueprint casting

--- Source/LyraALS/Animation/Lyra/LyraAnimInst.cpp
+++ Source/LyraALS/Animation/Lyra/LyraAnimInst.cpp
@@ -2,3 +2,40 @@
 
 
 #include "LyraAnimInst.h"
+
+#include "Player/LyraCharacter.h"
+
+// 动画实例代理
+void FLyraAnimInstProxy::PreUpdate(UAnimInstance* InAnimInstance, float DeltaSeconds)
+{
+	FAnimInstanceProxy::PreUpdate(InAnimInstance, DeltaSeconds);
+
+	ALyraCharacter* LyraCharacter = Cast<ALyraCharacter>(InAnimInstance->GetOwningActor());
+	if (!LyraCharacter) return;
+	UCharacterMovementComponent* MovementComp = LyraCharacter->GetCharacterMovement();
+	if (!MovementComp) return;
+	// 同步需要的数据到动画代理中
+}
+
+void FLyraAnimInstProxy::Update(float DeltaSeconds)
+{
+	FAnimInstanceProxy::Update(DeltaSeconds);
+}
+
+
+// 动画实例
+FAnimInstanceProxy* ULyraAnimInst::CreateAnimInstanceProxy()
+{
+	return &LyraAnimInstProxy;
+}
+
+void ULyraAnimInst::DestroyAnimInstanceProxy(FAnimInstanceProxy* InProxy)
+{
+	// Nothing to clean up since we are using a struct
+}
+
+// 动画评估后调用，运行于游戏线程
+void ULyraAnimInst::NativePostEvaluateAnimation()
+{
+	Super::NativePostEvaluateAnimation();
+}
--- Source/LyraALS/Animation/Lyra/LyraAnimInst.h
+++ Source/LyraALS/Animation/Lyra/LyraAnimInst.h
@@ -4,13 +4,39 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "Animation/AnimInstanceProxy.h"
 #include "LyraAnimInst.generated.h"
 
 /**
- * 
+ * 动画代理
  */
+
+USTRUCT()
+struct FLyraAnimInstProxy final : public FAnimInstanceProxy
+{
+	GENERATED_BODY()
+
+	FLyraAnimInstProxy() = default;
+
+	explicit FLyraAnimInstProxy(UAnimInstance* InAnimInstance) : FAnimInstanceProxy(InAnimInstance) {}
+
+protected:
+	virtual void PreUpdate(UAnimInstance* InAnimInstance, float DeltaSeconds) override;
+	virtual void Update(float DeltaSeconds) override;
+};
+
 UCLASS()
 class LYRAALS_API ULyraAnimInst : public UAnimInstance
 {
 	GENERATED_BODY()
+
+protected:
+	virtual FAnimInstanceProxy* CreateAnimInstanceProxy() override;
+	virtual void DestroyAnimInstanceProxy(FAnimInstanceProxy* InProxy) override;
+
+public:
+	virtual void NativePostEvaluateAnimation() override;
+private:
+	UPROPERTY(Transient)
+	FLyraAnimInstProxy LyraAnimInstProxy;
 };
